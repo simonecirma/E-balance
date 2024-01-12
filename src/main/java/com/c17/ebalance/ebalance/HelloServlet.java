7c18652e571b042e121c0259cdcb7f7f9dbaf546
AntonioDG30 <a.digiorgio8@studenti.unisa.it>
2024-01-12 20:18:54 +0100

    Prova Commit

--- src/main/java/com/c17/ebalance/ebalance/HelloServlet.java
+++ src/main/java/com/c17/ebalance/ebalance/HelloServlet.java
@@ -10,7 +10,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Ciao ciao ciao!";
+        message = "Hello World!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
