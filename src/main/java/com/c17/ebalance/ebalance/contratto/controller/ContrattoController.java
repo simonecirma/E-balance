b9b171d0fe1d4e1de65f6be031d74bb058d8ffd9
“AntonioDG30” <a.digiorgio8@studenti.unisa.it>
2024-01-16 11:40:33 +0100

    checkstyle

--- src/main/java/com/c17/ebalance/ebalance/contratto/controller/ContrattoController.java
+++ src/main/java/com/c17/ebalance/ebalance/contratto/controller/ContrattoController.java
@@ -1,6 +1,7 @@
 package com.c17.ebalance.ebalance.contratto.controller;
 
-import com.c17.ebalance.ebalance.contratto.service.*;
+import com.c17.ebalance.ebalance.contratto.service.ContrattoService;
+import com.c17.ebalance.ebalance.contratto.service.ContrattoServiceImpl;
 import com.c17.ebalance.ebalance.model.entity.contrattoBean;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -8,67 +9,64 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.sql.Date;
 import java.sql.SQLException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @WebServlet(name = "contrattoController", value = "/contrattoController")
-public class contrattoController extends HttpServlet {
+public class ContrattoController extends HttpServlet {
     private static final long serialVersionUID = 1L;
-    private contrattoService contrattoServ=new contrattoServiceImpl();
+    private ContrattoService contrattoService = new ContrattoServiceImpl();
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String action=request.getParameter("action");
+    public void doGet(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
+        String action = request.getParameter("action");
 
-        try{
-            if(action!=null){
-                if(action.equalsIgnoreCase("aggiornaContratto")) {
+        try {
+            if (action != null) {
+                if (action.equalsIgnoreCase("aggiornaContratto")) {
                     aggiornaContratto(request, response);
 
                 }
-                if(action.equalsIgnoreCase("aggiungiContratto")){
+                if (action.equalsIgnoreCase("aggiungiContratto")) {
                     aggiungiContratto(request, response);
 
                 }
-                List<contrattoBean> contratti=contrattoServ.visualizzaStoricoContratti();
+                List<contrattoBean> contratti = contrattoService.visualizzaStoricoContratti();
                 request.setAttribute("contratti", contratti);
-                contrattoBean contratto=contrattoServ.visualizzaContratto();
+                contrattoBean contratto = contrattoService.visualizzaContratto();
                 request.setAttribute("contratto", contratto);
                 RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/contratto.jsp");
                 dispatcher.forward(request, response);
-            }else{
-                List<contrattoBean> contratti=contrattoServ.visualizzaStoricoContratti();
+            } else {
+                List<contrattoBean> contratti = contrattoService.visualizzaStoricoContratti();
                 request.setAttribute("contratti", contratti);
-                contrattoBean contratto=contrattoServ.visualizzaContratto();
+                contrattoBean contratto = contrattoService.visualizzaContratto();
                 request.setAttribute("contratto", contratto);
                 RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/contratto.jsp");
                 dispatcher.forward(request, response);
             }
-        }catch(SQLException | ServletException e){
+        } catch (SQLException | ServletException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+    public void doPost(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
         doGet(request, response);
     }
 
-    public void aggiornaContratto(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        int id=Integer.parseInt(request.getParameter("idContratto"));
-        String ente=request.getParameter("nomeEnte");
-        float consumo=Float.parseFloat(request.getParameter("consumoMedioAnnuale"));
-        float costo=Float.parseFloat(request.getParameter("costoMedioUnitario"));
-        Date data=Date.valueOf(request.getParameter("dataSottoscrizione"));
-        int durata=Integer.parseInt(request.getParameter("durata"));
-        float prezzo=Float.parseFloat(request.getParameter("prezzoVendita"));
-        int admin=Integer.parseInt(request.getParameter("idAmministratore"));
+    public void aggiornaContratto(final HttpServletRequest request, final HttpServletResponse response) throws ServletException {
+        int id = Integer.parseInt(request.getParameter("idContratto"));
+        String ente = request.getParameter("nomeEnte");
+        float consumo = Float.parseFloat(request.getParameter("consumoMedioAnnuale"));
+        float costo = Float.parseFloat(request.getParameter("costoMedioUnitario"));
+        Date data = Date.valueOf(request.getParameter("dataSottoscrizione"));
+        int durata = Integer.parseInt(request.getParameter("durata"));
+        float prezzo = Float.parseFloat(request.getParameter("prezzoVendita"));
+        int admin = Integer.parseInt(request.getParameter("idAmministratore"));
 
-        contrattoBean bean=new contrattoBean();
+        contrattoBean bean = new contrattoBean();
 
         bean.setIdContratto(id);
         bean.setNomeEnte(ente);
@@ -78,17 +76,17 @@ public class contrattoController extends HttpServlet {
         bean.setDurata(durata);
         bean.setPrezzoVendita(prezzo);
         bean.setIdAmministatore(admin);
-        try{
-            contrattoServ.aggiornaContratto(bean);
-        }catch(SQLException e){
+        try {
+            contrattoService.aggiornaContratto(bean);
+        } catch (SQLException e) {
             e.printStackTrace();
             return;
         }
 
     }
 
-    public void aggiungiContratto(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        contrattoBean bean=new contrattoBean();
+    public void aggiungiContratto(final HttpServletRequest request, final HttpServletResponse response) throws IOException, ServletException {
+        contrattoBean bean = new contrattoBean();
         bean.setNomeEnte(request.getParameter("nomeEnte"));
         bean.setConsumoMedioAnnuale(Float.parseFloat(request.getParameter("consumoMedioAnnuale")));
         bean.setCostoMedioUnitario(Float.parseFloat(request.getParameter("costoMedioUnitario")));
@@ -96,9 +94,9 @@ public class contrattoController extends HttpServlet {
         bean.setDurata(Integer.parseInt(request.getParameter("durata")));
         bean.setPrezzoVendita(Float.parseFloat(request.getParameter("prezzoVendita")));
         bean.setIdAmministatore(Integer.parseInt(request.getParameter("idAmministratore")));
-        try{
-            contrattoServ.aggiungiContratto(bean);
-        }catch(SQLException e){
+        try {
+            contrattoService.aggiungiContratto(bean);
+        } catch (SQLException e) {
             e.printStackTrace();
             return;
         }
--- src/main/java/com/c17/ebalance/ebalance/contratto/service/ContrattoServiceImpl.java
+++ src/main/java/com/c17/ebalance/ebalance/contratto/service/ContrattoServiceImpl.java
@@ -6,11 +6,11 @@ import com.c17.ebalance.ebalance.model.DAO.contrattoDAO;
 import java.sql.SQLException;
 import java.util.List;
 
-public class contrattoServiceImpl implements contrattoService{
-    private contrattoDAO contrattoDao=new contrattoDAO();
+public class ContrattoServiceImpl implements ContrattoService {
+    private contrattoDAO contrattoDao = new contrattoDAO();
     @Override
-    public contrattoBean visualizzaContratto() throws SQLException{
-        return contrattoDao.visualizzaContratto();
+    public contrattoBean visualizzaContratto() throws SQLException {
+        return contrattoDAO.visualizzaContratto();
     }
 
     @Override
@@ -19,12 +19,12 @@ public class contrattoServiceImpl implements contrattoService{
     }
 
     @Override
-    public contrattoBean aggiornaContratto(contrattoBean contratto) throws SQLException {
+    public contrattoBean aggiornaContratto(final contrattoBean contratto) throws SQLException {
         return contrattoDao.aggiornaContratto(contratto);
     }
 
     @Override
-    public contrattoBean aggiungiContratto(contrattoBean contratto) throws SQLException {
+    public contrattoBean aggiungiContratto(final contrattoBean contratto) throws SQLException {
         return contrattoDao.aggiungiContratto(contratto);
     }
 }
