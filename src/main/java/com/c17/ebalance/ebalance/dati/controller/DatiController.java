acbe650f7c8471ed0941d4d3193cbce41b5efd56
daniipa00 <d.palma11@studenti.unisa.it>
2024-01-18 10:44:49 +0100

    -

--- src/main/java/com/c17/ebalance/ebalance/dati/controller/DatiController.java
+++ src/main/java/com/c17/ebalance/ebalance/dati/controller/DatiController.java
@@ -1,24 +1,24 @@
 package com.c17.ebalance.ebalance.dati.controller;
 
+import com.c17.ebalance.ebalance.dati.service.ConsumoService;
+import com.c17.ebalance.ebalance.dati.service.ConsumoServiceImpl;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(name = "DatiController", value = "/DatiController")
 public class DatiController extends HttpServlet {
 
     public void doGet(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
-
     }
 
     public void doPost(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
         doGet(request, response);
     }
-    
+
     public void destroy() {
     }
 }
